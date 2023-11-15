a1032bbd52ea20c1c1e147ae53f17129e1ae5126
Vishav Mahala <80857175+vishav-11@users.noreply.github.com>
2023-11-15 12:33:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+// --------------FOR header---------------
+
+function closebtn(){
+  document.getElementById("navLinks").style.right = "-200px";
+}
+
+function openbtn(){
+  document.getElementById("navLinks").style.right = "0";
+}
+
